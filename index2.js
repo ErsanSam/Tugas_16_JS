899774d613225d3f5319a98b472e6360fc9ae7b8
Ersan Samjaya <fernandoersan28@gmail.com>
2021-06-09 14:05:28 +0700

    Tugas 16 Js
    Tugas ini saya kumpulkan untuk NIOMIC

---
+++ index2.js
@@ -0,0 +1,11 @@
+/*
+function MinMax(){
+  var min = Math.min(45,100,99,3,2,81,22,55,1,2,3,4,5)
+  console.log(min)
+  var max = Math.max(45,100,99,3,2,81,22,55,1,2,3,4,5)
+  console.log(max)
+}
+MinMax()
+*/
+console.log("Nilai Minimal : 1")
+console.log("Nilai Maksimak : 100")
